5aec852ecd9c329c7a68c3e51e449733f47e75d4
Ankit.Vaghela <Ankit.Vaghela@IN-LT-17605>
2023-06-27 18:58:04 +0530

    Add project files.

---
+++ SeleniumTraining/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+
+namespace SeleniumTraining
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			char browser;
+			driverlaunch dl = new driverlaunch();
+			Console.WriteLine("Enter 'c' for chrome and 'e' for edge");
+			browser = Console.ReadLine()[0];
+
+			switch (browser)
+			{
+				case 'c':
+					dl.chrome();
+					break;
+				case 'e':
+					dl.edge();
+					break;
+				default:
+					Console.WriteLine("Please enter correct option");
+					break;
+
+			}
+		}
+	}
+}
+
---
+++ SeleniumTraining/driverlaunch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
+namespace SeleniumTraining
+{
+	internal class driverlaunch
+	{
+		public string url="https://computer-database.gatling.io/computers";
+		IWebElement addclick = null;
+		IWebDriver driver = null;
+		public void chrome()
+		{
+			driver = new ChromeDriver();
+			driver.Navigate().GoToUrl(url);
+			addclick = driver.FindElement(By.Id("add"));
+			addclick.Click();
+			driver.Quit();	
+        }
+
+		public void edge()
+		{
+			driver = new EdgeDriver();
+			driver.Navigate().GoToUrl(url);
+			addclick = driver.FindElement(By.Id("add"));
+			addclick.Click();
+			driver.Quit();
+		}
+
+
+	}
+}
