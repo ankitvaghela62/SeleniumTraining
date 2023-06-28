7cd74c37b900d11a763878986ce62f18a34f7d25
ankitvaghela62 <137903194+ankitvaghela62@users.noreply.github.com>
2023-06-28 16:40:45 +0530

    Update driverlaunch.cs
    Assignments

--- SeleniumTraining/driverlaunch.cs
+++ SeleniumTraining/driverlaunch.cs
@@ -15,7 +15,7 @@ namespace SeleniumTraining
 		public string url="https://computer-database.gatling.io/computers";
 		IWebElement addclick = null;
 		IWebDriver driver = null;
-		String cname = "selenium test";
+		String cname = "CompName";
 		String intro = "1998-02-22";
 		String disconti = "2000-12-02";
 		public void chrome()
@@ -36,13 +36,43 @@ namespace SeleniumTraining
 
 		}
 
-		public void edge()
-		{
-			driver = new EdgeDriver();
-			driver.Navigate().GoToUrl("https://echoecho.com/htmlforms10.htm");
-			addclick = driver.FindElement(By.XPath("/html/body/div[2]/table[9]/tbody/tr/td[4]/table/tbody/tr/td/div/span/form/table[3]/tbody/tr/td/table/tbody/tr/td/input[6]"));
-			addclick.Click();
+	
+
+
+		public void chromeentries()
+        {
+			driver = new ChromeDriver();
+			driver.Navigate().GoToUrl(url);
 			
+		
+			
+			
+			      for (int i = 0; i < 5; i++)
+				  
+			      {
+
+				        addclick = driver.FindElement(By.Id("add"));
+				        addclick.Click();      
+						IWebElement compname = driver.FindElement(By.XPath("//*[@id=\"name\"]"));
+						IWebElement intorduced = driver.FindElement(By.XPath("//*[@id=\"introduced\"]"));
+						IWebElement discontinue = driver.FindElement(By.XPath("/html/body/section/form/fieldset/div[3]/div/input"));
+						IWebElement company = driver.FindElement(By.XPath("/html/body/section/form/fieldset/div[4]/div/select"));
+						SelectElement cmp = new SelectElement(company);
+						compname.SendKeys(cname);
+						intorduced.SendKeys(intro);
+						discontinue.SendKeys(disconti);
+						cmp.SelectByIndex(4);
+						IWebElement btnsub = driver.FindElement(By.XPath("//*[@id=\"main\"]/form/div/input"));
+						btnsub.Click();
+				        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+
+				  }
+			Console.WriteLine("all the 5 computers are added!!!");
+
+			
+
+
 		}
 
 
