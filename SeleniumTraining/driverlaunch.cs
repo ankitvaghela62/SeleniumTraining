abda365891a1ce36675c43c062c3502ef12f1f5e
ankitvaghela62 <137903194+ankitvaghela62@users.noreply.github.com>
2023-06-28 12:10:05 +0530

    28 june changes

--- SeleniumTraining/driverlaunch.cs
+++ SeleniumTraining/driverlaunch.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumTraining
 {
@@ -14,22 +15,34 @@ namespace SeleniumTraining
 		public string url="https://computer-database.gatling.io/computers";
 		IWebElement addclick = null;
 		IWebDriver driver = null;
+		String cname = "selenium test";
+		String intro = "1998-02-22";
+		String disconti = "2000-12-02";
 		public void chrome()
 		{
 			driver = new ChromeDriver();
 			driver.Navigate().GoToUrl(url);
 			addclick = driver.FindElement(By.Id("add"));
 			addclick.Click();
-			driver.Quit();	
-        }
+			IWebElement compname = driver.FindElement(By.XPath("//*[@id=\"name\"]"));
+			IWebElement intorduced = driver.FindElement(By.XPath("//*[@id=\"introduced\"]"));
+			IWebElement discontinue = driver.FindElement(By.XPath("/html/body/section/form/fieldset/div[3]/div/input"));
+			IWebElement company = driver.FindElement(By.XPath("/html/body/section/form/fieldset/div[4]/div/select"));
+			SelectElement cmp = new SelectElement(company);
+			compname.SendKeys(cname);
+			intorduced.SendKeys(intro);
+			discontinue.SendKeys(disconti);
+			cmp.SelectByIndex(4);
+
+		}
 
 		public void edge()
 		{
 			driver = new EdgeDriver();
-			driver.Navigate().GoToUrl(url);
-			addclick = driver.FindElement(By.Id("add"));
+			driver.Navigate().GoToUrl("https://echoecho.com/htmlforms10.htm");
+			addclick = driver.FindElement(By.XPath("/html/body/div[2]/table[9]/tbody/tr/td[4]/table/tbody/tr/td/div/span/form/table[3]/tbody/tr/td/table/tbody/tr/td/input[6]"));
 			addclick.Click();
-			driver.Quit();
+			
 		}
 
 
